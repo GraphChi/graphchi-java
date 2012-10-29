d869a7888a58fe98e5378798faf34bfdc18562d1
Aapo Kyrola <akyrolacmu@gmail.com>
2012-10-29 07:11:57 -0700

    long converter
    --HG--
    branch : research

--- test/edu/cmu/graphchi/datablocks/TestLongConverter.java
+++ test/edu/cmu/graphchi/datablocks/TestLongConverter.java
@@ -16,7 +16,7 @@ public class TestLongConverter {
 
         long[] tests = new long[] {0, -5, 4, 81912, 839423, 92291, 10000, 2000000000,
                 Integer.MAX_VALUE, Integer.MIN_VALUE, Long.MAX_VALUE, Long.MIN_VALUE,
-                        8765432187654l, 80000000000l, 9999999999999l};
+                        8765432187654l, 80000000000l, 9999999999999l, -432197650000222l};
         for(long x : tests) {
             conv.setValue(arr, x);
             long y = conv.getValue(arr);
