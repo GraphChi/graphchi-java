567f4e9d416f6cd304d20a02d0c4fcc7d0639a03
Aapo Kyrola <akyrolacmu@gmail.com>
2012-12-12 13:57:59 -0800

    support directories
    --HG--
    branch : research

--- src/main/java/edu/cmu/graphchi/preprocessing/HDFSGraphLoader.java
+++ src/main/java/edu/cmu/graphchi/preprocessing/HDFSGraphLoader.java
@@ -3,10 +3,12 @@ package edu.cmu.graphchi.preprocessing;
 import edu.cmu.graphchi.LoggingInitializer;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
@@ -32,26 +34,37 @@ public class HDFSGraphLoader {
         Path inputPath = new Path(hdfsLocation);
 
         if (fs.isFile(inputPath)) {
-            FSDataInputStream in = fs.open(inputPath);
-            BufferedReader rd = new BufferedReader(new InputStreamReader(in));
-
-            String ln;
-            while ((ln = rd.readLine()) != null) {
-                if (ln.startsWith("#")) continue;
-                String[] tok = ln.split("\t");
-                if (tok.length == 2) {
-                    try {
-                        int from = Integer.parseInt(tok[0]);
-                        int to = Integer.parseInt(tok[1]);
-
-                        edgeProcessor.receiveEdge(from, to);
-                    } catch (NumberFormatException nfe) {
-                         logger.warning("Number format exceptions on line: " + ln);
-                    }
+            processFile(fs, inputPath);
+        } else {
+            FileStatus[] dirFiles = fs.listStatus(inputPath);
+            for(FileStatus status : dirFiles) {
+                logger.info("Dir entry: " + status.toString() + " " + status.getPath().getName());
+                if (status.getPath().getName().startsWith("part-")) {
+                    processFile(fs, status.getPath());
                 }
             }
-        } else {
-            throw new UnsupportedOperationException("Only loading of files currently supported");
+        }
+    }
+
+    private void processFile(FileSystem fs, Path inputPath) throws IOException {
+        logger.info("Process: " + inputPath);
+        FSDataInputStream in = fs.open(inputPath);
+        BufferedReader rd = new BufferedReader(new InputStreamReader(in));
+
+        String ln;
+        while ((ln = rd.readLine()) != null) {
+            if (ln.startsWith("#")) continue;
+            String[] tok = ln.split("\t");
+            if (tok.length == 2) {
+                try {
+                    int from = Integer.parseInt(tok[0]);
+                    int to = Integer.parseInt(tok[1]);
+
+                    edgeProcessor.receiveEdge(from, to);
+                } catch (NumberFormatException nfe) {
+                     logger.warning("Number format exceptions on line: " + ln);
+                }
+            }
         }
     }
 
