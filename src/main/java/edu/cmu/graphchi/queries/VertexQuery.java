aa3b3c6f8bcf7922f1ab8839e6a88b67345c0f79
Aapo Kyrola <akyrolacmu@gmail.com>
2013-02-06 15:24:50 -0800

    working friends-of-friends
    --HG--
    branch : graphquery

---
+++ src/main/java/edu/cmu/graphchi/queries/VertexQuery.java
@@ -0,0 +1,374 @@
+package edu.cmu.graphchi.queries;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.logging.Logger;
+
+import edu.cmu.graphchi.ChiFilenames;
+import edu.cmu.graphchi.ChiLogger;
+import edu.cmu.graphchi.ChiVertex;
+import edu.cmu.graphchi.datablocks.BytesToValueConverter;
+import edu.cmu.graphchi.datablocks.ChiPointer;
+import edu.cmu.graphchi.datablocks.DataBlockManager;
+import edu.cmu.graphchi.datablocks.FloatConverter;
+import edu.cmu.graphchi.io.CompressedIO;
+import edu.cmu.graphchi.preprocessing.VertexIdTranslate;
+import edu.cmu.graphchi.vertexdata.VertexIdValue;
+import ucar.unidata.io.RandomAccessFile;
+
+/**
+ * Disk-based queries of out-edges of a vertex.
+ * @author Aapo Kyrola
+ */
+public class VertexQuery {
+
+    private static final int NTHREADS = 4;
+
+    private static final Logger logger = ChiLogger.getLogger("vertexquery");
+    private ArrayList<Shard> shards;
+    private ExecutorService executor;
+
+    public VertexQuery(String baseFilename, int numShards) throws IOException{
+        shards = new ArrayList<Shard>();
+        for(int i=0; i<numShards; i++) {
+            shards.add(new Shard(baseFilename, i, numShards));
+        }
+        executor = Executors.newFixedThreadPool(NTHREADS);
+    }
+
+
+    public HashMap<Integer, Integer> queryOutNeighbors(final Collection<Integer> queryVertices) {
+        HashMap<Integer, Integer> results;
+        List<Future<HashMap<Integer, Integer>>> queryFutures = new ArrayList<Future<HashMap<Integer, Integer>>>();
+
+        /* Execute queries in parallel */
+        for(Shard shard : shards) {
+            final Shard _shard = shard;
+            queryFutures.add(executor.submit(new Callable<HashMap<Integer, Integer>>() {
+                @Override
+                public HashMap<Integer, Integer> call() throws Exception {
+                    HashMap<Integer, Integer> edges = _shard.query(queryVertices);
+                    return edges;
+                }
+            }));
+        }
+
+        /* Combine
+        */
+        long combineTime = 0;
+        try {
+            results = queryFutures.get(0).get();
+
+            for(int i=1; i < queryFutures.size(); i++) {
+                HashMap<Integer, Integer> shardResults = queryFutures.get(i).get();
+                long st = System.currentTimeMillis();
+
+                for(Map.Entry<Integer, Integer> e : shardResults.entrySet()) {
+                    if (results.containsKey(e.getKey())) {
+                        results.put(e.getKey(), e.getValue() + results.get(e.getKey()));
+                    } else {
+                        results.put(e.getKey(), e.getValue());
+                    }
+                }
+                combineTime += (System.currentTimeMillis() - st);
+            }
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+      //  System.out.println("Combine took " + combineTime + " ms");
+        return  results;
+    }
+
+    public HashSet<Integer> queryOutNeighbors(final int internalId) throws IOException  {
+        HashSet<Integer> friends;
+        List<Future<HashSet<Integer>>> queryFutures = new ArrayList<Future<HashSet<Integer>>>();
+
+        /* Query from shards in parallel */
+        for(Shard shard : shards) {
+
+            final Shard _shard = shard;
+            queryFutures.add(executor.submit(new Callable<HashSet<Integer>>() {
+                @Override
+                public HashSet<Integer> call() throws Exception {
+                    return _shard.query(internalId);
+                }
+            }));
+        }
+        try {
+            friends = queryFutures.get(0).get();
+
+            for(int i=1; i < queryFutures.size(); i++) {
+                HashSet<Integer> shardFriends = queryFutures.get(i).get();
+                for(Integer fr : shardFriends) {
+                    friends.add(fr);
+                }
+            }
+        } catch (Exception err) {
+            throw new RuntimeException(err);
+        }
+        return friends;
+    }
+
+    /**
+     * Shutdowns the executor threads.
+     */
+    public void shutdown() {
+        executor.shutdown();
+    }
+
+
+    static class Shard {
+        RandomAccessFile adjFile;
+        ShardIndex index;
+        int shardNum;
+        int numShards;
+        String fileName;
+
+
+        private Shard(String fileName, int shardNum, int numShards) throws IOException {
+            this.shardNum = shardNum;
+            this.numShards = numShards;
+            this.fileName = fileName;
+            File f = new File(ChiFilenames.getFilenameShardsAdj(fileName, shardNum, numShards));
+            adjFile = new RandomAccessFile(f.getAbsolutePath(), "r", 64 * 1024);
+            index = new ShardIndex(new File(f.getAbsolutePath() + ".index"));
+        }
+
+        /**
+         * Query efficiently all vertices
+         * @param queryIds
+         * @return
+         * @throws IOException
+         */
+        public HashMap<Integer, Integer> query(Collection<Integer> queryIds) throws IOException {
+            /* Sort the ids because the index-entries will be in same order */
+            ArrayList<Integer> sortedIds = new ArrayList<Integer>(queryIds);
+            Collections.sort(sortedIds);
+
+            ArrayList<IndexEntry> indexEntries = new ArrayList<IndexEntry>(sortedIds.size());
+            for(Integer a : sortedIds) {
+                indexEntries.add(index.lookup(a));
+            }
+
+            HashMap<Integer, Integer> results = new HashMap<Integer, Integer>(5000);
+            IndexEntry entry = null, lastEntry = null;
+            int curvid=0, adjOffset=0;
+            for(int qIdx=0; qIdx < sortedIds.size(); qIdx++) {
+                entry = indexEntries.get(qIdx);
+                int vertexId = sortedIds.get(qIdx);
+
+                /* If consecutive vertices are in same indexed block, i.e their
+                   index entries are the same, then we just continue.
+                 */
+                if (qIdx == 0 || !entry.equals(lastEntry))   {
+                    curvid = entry.vertex;
+                    adjOffset = entry.fileOffset;
+                    adjFile.seek(adjOffset);
+                }
+                while(curvid <= vertexId) {
+                    int n;
+                    int ns = adjFile.readUnsignedByte();
+                    assert(ns >= 0);
+                    adjOffset++;
+
+                    if (ns == 0) {
+                        curvid++;
+                        int nz = adjFile.readUnsignedByte();
+
+                        adjOffset++;
+                        assert(nz >= 0);
+                        curvid += nz;
+                        continue;
+                    }
+
+                    if (ns == 0xff) {
+                        n = adjFile.readInt();
+                        adjOffset += 4;
+                    } else {
+                        n = ns;
+                    }
+
+                    if (curvid == vertexId) {
+                        while (--n >= 0) {
+                            int target = adjFile.readInt();
+                            Integer curCount = results.get(target);
+                            if (curCount == null) {
+                                results.put(target, 0);
+                            } else {
+                                results.put(target, 1 + curCount);
+                            }
+                        }
+                    } else {
+                        adjFile.skipBytes(n * 4);
+                    }
+                    curvid++;
+                }
+            }
+            return results;
+        }
+
+        public HashSet<Integer> query(int vertexId) throws IOException {
+            return new HashSet<Integer>(query(Collections.singletonList(vertexId)).keySet());
+        }
+
+        public <VT> List<VertexIdValue<VT>> queryWithValues(int vertexId, BytesToValueConverter<VT> conv) throws
+                IOException {
+
+            List<VertexIdValue<VT>> results = new ArrayList<VertexIdValue<VT>>();
+            IndexEntry entry = index.lookup(vertexId);
+
+            int curvid = entry.vertex;
+            int adjOffset = entry.fileOffset;
+            int edgeOffset = entry.edgePointer;
+            String edataShardName = ChiFilenames.getFilenameShardEdata(fileName, conv, shardNum, numShards);
+            int fileSize = ChiFilenames.getShardEdataSize(edataShardName);
+
+
+            adjFile.seek(adjOffset);
+
+            /* Edge data block*/
+            int blockSize = ChiFilenames.getBlocksize(conv.sizeOf());
+
+            byte[] edgeDataBlock = new byte[blockSize];
+            int curBlockId = (-1);
+            byte[] tmp = new byte[conv.sizeOf()];
+
+            while(curvid <= vertexId) {
+                int n;
+                int ns = adjFile.readUnsignedByte();
+                assert(ns >= 0);
+                adjOffset++;
+
+                if (ns == 0) {
+                    curvid++;
+                    int nz = adjFile.readUnsignedByte();
+
+                    adjOffset++;
+                    assert(nz >= 0);
+                    curvid += nz;
+                    continue;
+                }
+
+                if (ns == 0xff) {
+                    n = adjFile.readInt();
+                    adjOffset += 4;
+                } else {
+                    n = ns;
+                }
+
+                if (curvid == vertexId) {
+                    while (--n >= 0) {
+                        int target = adjFile.readInt();
+
+                        int blockId = edgeOffset * conv.sizeOf() / blockSize;
+                        if (blockId != curBlockId) {
+                            String blockFileName = ChiFilenames.getFilenameShardEdataBlock(
+                                    edataShardName,
+                                    blockId, blockSize);
+                            curBlockId = blockId;
+                            int len = Math.min(blockSize, fileSize - blockId * blockSize);
+                            CompressedIO.readCompressed(new File(blockFileName), edgeDataBlock, len);
+                        }
+                        System.arraycopy(edgeDataBlock, (edgeOffset * conv.sizeOf()) % blockSize, tmp, 0, conv.sizeOf());
+                        VT value = conv.getValue(tmp);
+                        results.add(new VertexIdValue<VT>(target, value));
+                        edgeOffset++;
+                    }
+                } else {
+                    adjFile.skipBytes(n * 4);
+                    edgeOffset += n;
+                }
+                curvid++;
+            }
+            return results;
+        }
+
+
+    }
+
+    static class ShardIndex {
+        File indexFile;
+        int[] vertices;
+        int[] edgePointer;
+        int[] fileOffset;
+
+        ShardIndex(File indexFile) throws IOException {
+            this.indexFile = indexFile;
+            load();
+        }
+
+        private void load() throws IOException {
+            int n = (int) (indexFile.length() / 12) + 1;
+            vertices = new int[n];
+            edgePointer = new int[n];
+            fileOffset = new int[n];
+
+            vertices[0] = 0;
+            edgePointer[0] = 0;
+            fileOffset[0] = 0;
+
+            DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(indexFile)));
+            int i = 1;
+            while (i < n) {
+                vertices[i] = dis.readInt();
+                fileOffset[i] = dis.readInt();
+                edgePointer[i] = dis.readInt();
+                i++;
+            }
+        }
+
+        IndexEntry lookup(int vertexId) {
+            int idx = Arrays.binarySearch(vertices, vertexId);
+            if (idx >= 0) {
+                return new IndexEntry(vertexId, edgePointer[idx], fileOffset[idx]);
+            } else {
+                idx = -(idx + 1) - 1;
+                return new IndexEntry(vertices[idx], edgePointer[idx], fileOffset[idx]);
+            }
+
+        }
+
+    }
+
+    static class IndexEntry {
+        int vertex, edgePointer, fileOffset;
+
+        IndexEntry(int vertex, int edgePointer, int fileOffset) {
+            this.vertex = vertex;
+            this.edgePointer = edgePointer;
+            this.fileOffset = fileOffset;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            IndexEntry that = (IndexEntry) o;
+
+            if (edgePointer != that.edgePointer) return false;
+            if (fileOffset != that.fileOffset) return false;
+            if (vertex != that.vertex) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = vertex;
+            result = 31 * result + edgePointer;
+            result = 31 * result + fileOffset;
+            return result;
+        }
+
+        public String toString() {
+            return "vertex: " + vertex + ", offset=" + fileOffset;
+        }
+    }
+
+
+}
---
+++ src/main/java/edu/cmu/graphchi/queries/demo/FriendsOfFriends.java
@@ -0,0 +1,203 @@
+package edu.cmu.graphchi.queries.demo;
+
+import edu.cmu.graphchi.ChiFilenames;
+import edu.cmu.graphchi.ChiLogger;
+import edu.cmu.graphchi.datablocks.FloatConverter;
+import edu.cmu.graphchi.preprocessing.VertexIdTranslate;
+import edu.cmu.graphchi.queries.VertexQuery;
+import edu.cmu.graphchi.util.MultinomialSampler;
+import edu.cmu.graphchi.vertexdata.VertexAggregator;
+import edu.cmu.graphchi.vertexdata.VertexIdValue;
+
+import java.io.*;
+import java.util.*;
+import java.util.logging.Logger;
+
+/**
+ * Demonstration of the query capabilities of GraphChi.
+ * With this app, after you have computed PageRank for each vertex,
+ * you can make simple friends-recommendations queries: querying for user X
+ * will recommend the top friends (out-edges) of user X's friends (out-edges).
+ * @author Aapo Kyrola
+ */
+public class FriendsOfFriends {
+
+    private VertexQuery queryEngine;
+    private static final Logger logger = ChiLogger.getLogger("fof");
+    private VertexIdTranslate translator;
+    private String baseFilename;
+    private boolean weightByPagerank;
+    private int numShards;
+    private float[] ranks;  // Note: ranks are by the *internal vertex id*.
+    private BufferedWriter logWriter = new BufferedWriter(new FileWriter("fof.log"));
+
+    /**
+     * Construct friends-of-friends (of followees of followees in Twitter parlance)
+     * engine.
+     * @param baseFilename graph name
+     * @param numShards number of shards
+     * @param weightByPagerank whether to weight sampling by pagerank
+     * @throws IOException
+     */
+    public FriendsOfFriends(String baseFilename, int numShards, boolean weightByPagerank) throws IOException {
+        this.queryEngine = new VertexQuery(baseFilename, numShards);
+        this.baseFilename = baseFilename;
+        this.weightByPagerank = weightByPagerank;
+        this.numShards = numShards;
+        this.translator = VertexIdTranslate.fromFile(new File(ChiFilenames.getVertexTranslateDefFile(baseFilename, numShards)));
+
+        if (weightByPagerank) {
+            loadRanks();
+        }
+    }
+
+    private void loadRanks() throws IOException {
+        logger.info("Loading ranks...");
+        long st = System.currentTimeMillis();
+        int numVertices = ChiFilenames.numVertices(baseFilename, numShards);
+        ranks = new float[numVertices];
+        Iterator<VertexIdValue<Float>> iter = VertexAggregator.vertexIterator(numVertices, baseFilename,
+                new FloatConverter(), VertexIdTranslate.identity());
+        while(iter.hasNext()) {
+            VertexIdValue<Float> idVal = iter.next();
+            ranks[idVal.getVertexId()] = idVal.getValue();
+        }
+        logger.info("Loaded ranks to memory in " + (System.currentTimeMillis() - st) + " ms");
+    }
+
+    /**
+     * Recommend friends
+     * @param vertexId  query vertex
+     * @param fanOut maximum of query vertex's friends to consider. Selected randomly.
+     * @throws IOException
+     */
+    public void recommendFriends(int vertexId, int fanOut) throws IOException {
+        int internalId = translator.forward(vertexId);
+
+        logger.info("Querying for " + namify(vertexId) + " --> " + internalId);
+
+        long stTime = System.currentTimeMillis();
+
+        int total = 0;
+        HashMap<Integer, Integer> friendsOfFriends;
+        final HashSet<Integer> friends = queryEngine.queryOutNeighbors(internalId);
+
+        long t = System.currentTimeMillis() - stTime;
+        logger.info("Found " + friends.size() + " friends  in " + t + " ms.");
+
+
+
+        Random r = new Random();
+        if (friends.size() > fanOut) {
+
+            ArrayList<Integer> friendsAll = new ArrayList<Integer>(friends);
+            friends.clear();
+
+            if (ranks != null) {
+                // Going to sample by PageRank
+                float[] weights = new float[friendsAll.size()];
+                for(int i=0; i<weights.length; i++) {
+                    weights[i] = ranks[friendsAll.get(i)];
+                }
+                int[] samples = MultinomialSampler.generateSamplesAliasMethod(r, weights, fanOut * 2);
+                for(int i : samples) {
+                    friends.add(friendsAll.get(i));
+                    if (friends.size() == fanOut) break;
+                }
+            } else {
+                for(int i=0; i < fanOut; i++) friends.add(friendsAll.get(Math.abs(r.nextInt()) % friendsAll.size()));
+            }
+        }
+
+        if (friends.size() == 0) {
+            return;
+        }
+
+        stTime = System.currentTimeMillis();
+
+        friendsOfFriends = queryEngine.queryOutNeighbors(friends);
+
+        long t2 = (System.currentTimeMillis() - stTime);
+
+        for(int friend : friends) friendsOfFriends.remove(friend);
+        logger.info("Found " + friendsOfFriends.size() + " friends-of-friends (that are not friends) in " +
+                t2 + "ms");
+
+        /* Take only top */
+        int k = 10;
+        TreeMap<Integer, Integer> counts = new TreeMap<Integer, Integer>();
+        for(Map.Entry<Integer, Integer> e : friendsOfFriends.entrySet()) {
+            if (counts.size() < k) {
+                counts.put(-e.getValue(), e.getKey());
+            } else {
+                int smallest = counts.lastKey();
+                if (-e.getValue() < smallest) {
+                    counts.remove(counts.lastKey());
+                    counts.put(-e.getValue(), translator.backward(e.getKey()));
+                }
+            }
+        }
+
+        for(Map.Entry<Integer, Integer> top : counts.entrySet()) {
+            System.out.println(namify(top.getValue()) + " : " + (-top.getKey()));
+        }
+
+        logWriter.write(friends.size() + "," + t + "," + t2 + "\n");
+
+    }
+
+    private String namify(Integer value) throws IOException {
+        File f = new File(new File(baseFilename).getParent(), "twitter_names.dat");
+        if (!f.exists()) {
+            return value+"";
+        }
+        int i = value * 16;
+        RandomAccessFile raf = new RandomAccessFile(f.getAbsolutePath(), "r");
+        raf.seek(i);
+        byte[] tmp = new byte[16];
+        raf.read(tmp);
+        raf.close();
+        return new String(tmp) + "(" + value + ")";
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        String baseFilename = args[0];
+        int numShards = Integer.parseInt(args[1]);
+
+        FriendsOfFriends fof = new FriendsOfFriends(baseFilename, numShards, true);
+
+        BufferedReader cmdIn = new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+            System.out.print("Enter vertex id to query >> :: ");
+            String ln = cmdIn.readLine();
+            if (ln.startsWith("q")) break;
+
+            if (ln.startsWith("t")) {
+                for(int i=10; i < 1000; i++) {
+                    fof.recommendFriends(i, 100);
+                }
+                break;
+            }
+            if (ln.startsWith("b")) {
+                // Benchmark
+                int numVertices = ChiFilenames.numVertices(baseFilename, numShards);
+
+                Random r = new Random();
+                for(int i=0; i<1000000; i++) {
+                    int vId = r.nextInt(numVertices);
+                    fof.recommendFriends(vId, 100);
+
+                    if (i % 1000 == 0) {
+                        logger.info("Benchmark round " + i);
+                    }
+                    fof.logWriter.flush();
+                }
+            }
+
+            int queryId = Integer.parseInt(ln);
+            fof.recommendFriends(queryId, 100);
+        }
+        fof.queryEngine.shutdown();
+    }
+}
